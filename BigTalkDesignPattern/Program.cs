5aa643182138f927c04f7217db2611c60c62acc1
HFYEH <sharefun010407@gmail.com>
2019-01-06 04:17:10 +0800

    Chapter1 change from if to switch

--- BigTalkDesignPattern/Program.cs
+++ BigTalkDesignPattern/Program.cs
@@ -10,44 +10,42 @@ namespace SimpleFactory
             {
                 Console.WriteLine("Please enter number A:");
 
-                string A = Console.ReadLine();
+                string strNumberA = Console.ReadLine();
 
                 Console.WriteLine("Please enter number operator (+,-,*,/)");
 
-                string Operator = Console.ReadLine();
+                string strOperator = Console.ReadLine();
 
                 Console.WriteLine("Please enter number B:");
 
-                string B = Console.ReadLine();
+                string strNumberB = Console.ReadLine();
 
                 string Result;
 
-                if (Operator == "+")
+                switch (strOperator)
                 {
-                    Result = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(B));
-                }
-                else if (Operator == "-")
-                {
-                    Result = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(B));
-                }
-                else if (Operator == "*")
-                {
-                    Result = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(B));
-                }
-                else if (Operator == "/")
-                {
-                    if (Math.Abs( Convert.ToDouble(B) - 0.0 ) < 0.0001)
-                    {
-                        throw new Exception("Cannot be zero!");
-                    }
-                    else
-                    {
-                        Result = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(B));
-                    }
-                }
-                else
-                {
-                    Result = "None";
+                    case "+":
+                        Result = Convert.ToString(Convert.ToDouble(strNumberA) + Convert.ToDouble(strNumberB));
+                        break;
+                    case "-":
+                        Result = Convert.ToString(Convert.ToDouble(strNumberA) - Convert.ToDouble(strNumberB));
+                        break;
+                    case "*":
+                        Result = Convert.ToString(Convert.ToDouble(strNumberA) * Convert.ToDouble(strNumberB));
+                        break;
+                    case "/":
+                        if (Math.Abs(Convert.ToDouble(strNumberB) - 0.0) < 0.0001)
+                        {
+                            throw new Exception("Cannot be zero!");
+                        }
+                        else
+                        {
+                            Result = Convert.ToString(Convert.ToDouble(strNumberA) / Convert.ToDouble(strNumberB));
+                        }
+                        break;
+                    default:
+                        Result = "None";
+                        break;
                 }
 
                 Console.WriteLine("The result is: " + Result);
