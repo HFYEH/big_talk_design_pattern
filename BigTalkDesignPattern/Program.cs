9e674c2905268616991cdb63546af57517672aee
HFYEH <sharefun010407@gmail.com>
2019-01-05 22:24:36 +0800

    Chapter1 start

---
+++ BigTalkDesignPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BigTalkDesignPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number A:");
+
+            string A = Console.ReadLine();
+
+            Console.WriteLine("Please enter number operator (+,-,*,/)");
+
+            string Operator = Console.ReadLine();
+
+            Console.WriteLine("Please enter number B:");
+
+            string B = Console.ReadLine();
+
+            string Result;
+
+            if (Operator == "+")
+            {
+                Result = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(B));
+            }
+            else if (Operator == "-")
+            {
+                Result = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(B));
+            }
+            else if (Operator == "*")
+            {
+                Result = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(B));
+            }
+            else if (Operator == "/")
+            {
+                Result = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(B));
+            }
+            else
+            {
+                Result = "None";
+            }
+
+            Console.WriteLine("The result is: " + Result);
+        }
+    }
+}
