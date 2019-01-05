14883648340387bcb5f07044054c6065ad816268
HFYEH <sharefun010407@gmail.com>
2019-01-05 22:39:20 +0800

    Chapter1 add error handling

--- BigTalkDesignPattern/Program.cs
+++ BigTalkDesignPattern/Program.cs
@@ -6,42 +6,56 @@ namespace BigTalkDesignPattern
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter number A:");
-
-            string A = Console.ReadLine();
-
-            Console.WriteLine("Please enter number operator (+,-,*,/)");
-
-            string Operator = Console.ReadLine();
-
-            Console.WriteLine("Please enter number B:");
-
-            string B = Console.ReadLine();
-
-            string Result;
-
-            if (Operator == "+")
+            try
             {
-                Result = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(B));
-            }
-            else if (Operator == "-")
-            {
-                Result = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(B));
-            }
-            else if (Operator == "*")
-            {
-                Result = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(B));
-            }
-            else if (Operator == "/")
-            {
-                Result = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(B));
-            }
-            else
-            {
-                Result = "None";
-            }
+                Console.WriteLine("Please enter number A:");
 
-            Console.WriteLine("The result is: " + Result);
+                string A = Console.ReadLine();
+
+                Console.WriteLine("Please enter number operator (+,-,*,/)");
+
+                string Operator = Console.ReadLine();
+
+                Console.WriteLine("Please enter number B:");
+
+                string B = Console.ReadLine();
+
+                string Result;
+
+                if (Operator == "+")
+                {
+                    Result = Convert.ToString(Convert.ToDouble(A) + Convert.ToDouble(B));
+                }
+                else if (Operator == "-")
+                {
+                    Result = Convert.ToString(Convert.ToDouble(A) - Convert.ToDouble(B));
+                }
+                else if (Operator == "*")
+                {
+                    Result = Convert.ToString(Convert.ToDouble(A) * Convert.ToDouble(B));
+                }
+                else if (Operator == "/")
+                {
+                    if (Math.Abs( Convert.ToDouble(B) - 0.0 ) < 0.0001)
+                    {
+                        throw new Exception("Cannot be zero!");
+                    }
+                    else
+                    {
+                        Result = Convert.ToString(Convert.ToDouble(A) / Convert.ToDouble(B));
+                    }
+                }
+                else
+                {
+                    Result = "None";
+                }
+
+                Console.WriteLine("The result is: " + Result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is an error: " + ex.Message);
+            }
         }
     }
 }
